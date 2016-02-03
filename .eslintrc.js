fb972b4b24708ca4096232f3d5ce67118678e8ad
Roger <mr.rsmith15@gmail.com>
2016-02-02 22:23:18 -0500

    Added Linter

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "rules": {
+        "indent": [
+            2,2
+        ],
+        "no-console": [0],
+        "no-undef":[0],
+        "quotes": [
+            2,
+            "single"
+        ],
+        "linebreak-style": [
+            2,
+            "windows"
+        ],
+        "semi": [
+            2,
+            "never"
+        ]
+    },
+    "env": {
+        "es6": true,
+        "browser": false
+    },
+    "extends": "eslint:recommended"
+};
